b3a4a69941d1b993a0c44af701fd0ec119fe062e
Corey Coole <coreycoole@gmail.com>
2019-12-02 14:08:59 -0700

    added aws deploy scripts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a(6z$i^9_f6l$f+*p5vkj+i7(tvf^(2p+=m7xck$&c04#yg3q^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -131,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
