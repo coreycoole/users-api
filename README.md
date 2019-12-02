d8d85956171421cfc3ff16fa8c7263eccfa1c0dd
Corey Coole <coreycoole@gmail.com>
2019-12-02 00:56:21 -0700

    updates django-filters

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # User Profile Django REST api
 
-User profiles API source code .
+User profiles API source code.
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters 
 
 
 from .models import UserProfile
