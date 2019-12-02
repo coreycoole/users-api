9fa1defd228cfa3bc455f374a9108969159240af
Corey Coole <coreycoole@gmail.com>
2019-12-02 12:22:18 -0700

    updated django filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
-from django_filters import rest_framework as filters 
+from django_filters import rest_framework as filters
 
 
 from .models import UserProfile
@@ -12,7 +12,7 @@ from .serializers import ProfileSerializer
 class ProfileListAPIView(ListAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = ProfileSerializer
-
+    filter_backends = (filters.DjangoFilterBackend,)
     filter_fields = {
         'login_count': ['gt', 'lte']
     }
